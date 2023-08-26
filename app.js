a777240123c7c6979015e571a498fe6c6c54a165
Tresten Pool <trestenpool@gmail.com>
2023-08-26 17:39:07 -0500

    trying sample code

--- app.js
+++ app.js
@@ -1,44 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-const port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
-const server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        let body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
 
 // /** Configure our app **/
